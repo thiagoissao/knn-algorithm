cb89c60499352e2e4974562b0b4c2c7d8d730e5e
Thiago Yasunaka <thiagoyasunaka@hotmail.com>
2020-10-26 17:03:18 -0300

    transorma cada linha em um array

--- js-knn/main.js
+++ js-knn/main.js
@@ -1,6 +1,18 @@
 const lineReader = require('line-reader')
 
 const PATH = '../bases/test_59.data'
-lineReader.eachLine(PATH, (line, last) => {
-  console.log(line)
-})
+
+const getClass = (data, k) => data[k]
+
+const init = (path, k) => {
+  lineReader.eachLine(path, (line, last) => {
+    const data = line.split(',')
+    const knnClass = getClass(data, k)
+    console.log(knnClass)
+  }, err => {
+    if (err) console.error(err)
+  })
+}
+
+//Inicia nessa função
+init(PATH, 59)
