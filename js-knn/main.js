70f6d74bf82a093664037325086318ebd3158998
Thiago Yasunaka <thiagoyasunaka@hotmail.com>
2020-10-29 08:31:08 -0300

    cálculo da distancia euclidiana

--- js-knn/main.js
+++ js-knn/main.js
@@ -1,18 +1,45 @@
-const lineReader = require('line-reader')
+const TRAIN_PATH = '../bases/train_59_small.data'
+const TEST_PATH = '../bases/test_59_small.data'
+const K = 10
 
-const PATH = '../bases/test_59.data'
+const fs = require('fs')
 
-const getClass = (data, k) => data[k]
-
-const init = (path, k) => {
-  lineReader.eachLine(path, (line, last) => {
-    const data = line.split(',')
-    const knnClass = getClass(data, k)
-    console.log(knnClass)
-  }, err => {
-    if (err) console.error(err)
-  })
+const init = (trainPath, testPath) => {
+  let trainDatabase = fs.readFileSync(trainPath, { encoding: 'utf8', flag: 'r' }).split('\n')
+  trainDatabase.pop() //remove pois é uma string vazia por causa da última linha do arquivo
+  let testDatabase = fs.readFileSync(testPath, { encoding: 'utf8', flag: 'r' }).split('\n')
+  testDatabase.pop() //remove pois é uma string vazia por causa da última linha do arquivo
+  return {
+    train: trainDatabase,
+    test: testDatabase
+  }
 }
 
-//Inicia nessa função
-init(PATH, 59)
+const execute = (databases, k) => {
+  const { train, test } = databases
+  let trainClasses = []
+
+  const sortedDistances = test.map(rowTest => {
+    const itemTest = rowTest.split(',')
+    itemTest.pop() //Remove class
+    const exponentiations = train.map(rowTrain => {
+      const itemTrain = rowTrain.split(',')
+      trainClasses.push(itemTrain.pop()) //Remove class
+      return itemTrain.map((columnTrain, index) => {
+        if (typeof +columnTrain === 'number' && typeof +itemTest === 'number') {
+          const subtract = itemTest[index] - columnTrain
+          return Math.pow(subtract, 2)
+        }
+        return 0
+      })
+    })
+    const euclidianaDistances = exponentiations.map(exp => Math.sqrt(exp.reduce((acc, cur) => acc += cur, 0)))
+    return euclidianaDistances.sort()
+  })
+
+  console.log(sortedDistances)
+}
+
+//Chamadas das funções
+const databases = init(TRAIN_PATH, TEST_PATH)
+execute(databases, K)
